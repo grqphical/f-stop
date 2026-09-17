e928ba75aef1850a272ff942c3c9efb8fa228b82
Nathan Jacobson <nathan@nathanjacobson.ca>
2026-09-16 17:16:00 -0700

    added Vue router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router';
 
 fetch("/api/v1/health").then(async (response) => console.log(await response.json()))
 </script>
 
 <template>
-  <HelloWorld />
+  <RouterView />
 </template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import HomeView from './Views/HomeView.vue'
+
+const routes = [
+    {
+        path: '/',
+        component: HomeView,
+    }
+]
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
