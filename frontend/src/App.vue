757f7e4b418a77413543d876cdad2ad39fd47a56
Nathan Jacobson <nathan@nathanjacobson.ca>
2026-09-16 17:11:48 -0700

    got basic frontend created, added proxy support for API

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+fetch("/api/v1/health").then(async (response) => console.log(await response.json()))
+</script>
+
+<template>
+  <HelloWorld />
+</template>
